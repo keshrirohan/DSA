10dee98d58b6a08f7e9ff31af6e022e56b78d9a9
keshrirohan <rohankeshri06@gmail.com>
2026-04-21 20:58:21 +0530

    Refactor rotate function in leet189.cpp to improve variable handling

--- 150 q/leet189.cpp
+++ 150 q/leet189.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-         k=k%nums.size();
+        int n=nums.size(); 
+        k=k%n;
+
         
         reverse(nums.begin(),nums.end());
         reverse(nums.begin(),nums.begin()+k);
