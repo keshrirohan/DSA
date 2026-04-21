33f6a911438fdd4180934abccc0d534acb9868fd
keshrirohan <keshrirohan214@gmail.com>
2026-04-21 19:27:36 +0530

    Refactor rotate function in leet189.cpp to simplify logic and remove unnecessary conditions

--- 150 q/leet189.cpp
+++ 150 q/leet189.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        if(k==0|| k== nums.size()){
-            return;
-        }
-       reverse(nums.begin(),nums.end());
+         k=k%nums.size();
+        
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
         reverse(nums.begin()+k,nums.end());
-        if(nums.size()%2==0){
-        reverse(nums.begin(),nums.end()-k);
-        }else{
-        reverse(nums.begin(),nums.end()-k-1);
-          }  }
+    }
 };
