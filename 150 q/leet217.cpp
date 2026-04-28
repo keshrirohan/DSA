993ef70b759f6b116f3dc112705eaa8b6a20c21b
keshrirohan <rohankeshri06@gmail.com>
2026-04-28 17:59:34 +0530

    Add second approach for containsDuplicate function in leet217.cpp

--- 150 q/leet217.cpp
+++ 150 q/leet217.cpp
@@ -12,3 +12,21 @@ public:
         return false;
     }
 };
+
+
+//approach 2
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(auto num:nums){
+            mp[num]++;
+        }
+        for(auto m:mp){
+            if(m.second>1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
