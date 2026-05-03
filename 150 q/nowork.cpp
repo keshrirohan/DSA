1f4b11b8a9a220de92a6f77e2e313f475859d41c
keshrirohan <rohankeshri06@gmail.com>
2026-05-03 22:59:48 +0530

    Update comments in nowork.cpp for clarity and date accuracy

--- 150 q/nowork.cpp
+++ 150 q/nowork.cpp
@@ -1,1 +1,2 @@
 // today date is 27-4-26 and i am unable to do anything due to my health issue 
+// today is 03-5-2026 and i uanble to do anything only the revision take place of the question because today is sunday 
