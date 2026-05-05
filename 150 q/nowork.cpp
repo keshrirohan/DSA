e2450ecd0dd785763459c7785df3e49061d9500d
keshrirohan <rohankeshri06@gmail.com>
2026-05-05 23:58:06 +0530

    Update notes in nowork.cpp for clarity and completeness

--- 150 q/nowork.cpp
+++ 150 q/nowork.cpp
@@ -1,3 +1,4 @@
 // today date is 27-4-26 and i am unable to do anything due to my health issue 
 // today is 03-5-2026 and i uanble to do anything only the revision take place of the question because today is sunday 
 // no qustion due to practical today is 04-5-2026 
+// today is 05-5-2026 and due to exam i am unable to do anything
