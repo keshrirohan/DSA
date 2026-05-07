3b17f1d922dd45593365597a1cfad8021ca707f1
keshrirohan <rohankeshri06@gmail.com>
2026-05-08 00:14:38 +0530

    Add optimal implementation of findMin function using binary search in leet153.cpp

--- 150 q/leet153.cpp
+++ 150 q/leet153.cpp
@@ -11,3 +11,22 @@ public:
         return nums[pivot];
     }
 };
+
+// optimal approach using log n time complexity
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int s = 0, e = nums.size() - 1, mid = nums[s];
+       
+        
+        while (s < e) {
+            mid=s+(e-s)/2;
+            if(nums[mid]>nums[e]){
+                s=mid+1;
+            }else{
+                e=mid;
+            }
+                    }
+        return nums[s];
+    }
+};
