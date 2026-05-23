39ad3d9550c107496daec696e958b5eeb5fa8e2a
keshrirohan <rohankeshri06@gmail.com>
2026-05-23 19:38:29 +0530

    Add alternative implementation of findPeakElement function in leet162.cpp

--- 150 q/leet162.cpp
+++ 150 q/leet162.cpp
@@ -19,3 +19,25 @@ public:
         return e;
             }
 };
+
+
+//difficult approach a bit 
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int s = 0, e = nums.size() - 1;
+        int mid;
+        while (s < e) {
+            mid = s + (e - s) / 2;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) &&
+                (mid == nums.size() - 1 || nums[mid] > nums[mid + 1])) {
+                return mid;
+            } else if (nums[mid] > nums[mid + 1]) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return s;
+    }
+};
