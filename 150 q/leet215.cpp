bea05d35e5d9d94eb35309e22bc2be4914702873
keshrirohan <rohankeshri06@gmail.com>
2026-05-24 21:43:50 +0530

    Add optimal implementation of findKthLargest function using min heap in leet215.cpp

--- 150 q/leet215.cpp
+++ 150 q/leet215.cpp
@@ -22,3 +22,24 @@ public:
         return maxval;
     }
 };
+
+
+
+
+// this is the optimal approach using min heap the time complexity is O(nlogk) and space complexity is O(k)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int ,vector<int>, greater<int>> h;
+        for (int i = 0; i < k; i++) {
+            h.push(nums[i]);
+        }
+        for (int i = k; i < nums.size(); i++) {
+           if(nums[i]>h.top()){
+            h.pop();
+            h.push(nums[i]);
+           }
+        }
+        return h.top();
+    }
+};
